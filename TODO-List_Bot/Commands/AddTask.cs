5d25976097fae721299478c36a26e2fb3269430d
ADavlatov <a@davlatov.space>
2022-08-01 06:34:44 +0300

    Добавление новых тасков

--- TODO-List_Bot/Commands/AddTask.cs
+++ TODO-List_Bot/Commands/AddTask.cs
@@ -1,6 +1,42 @@
+using Microsoft.Extensions.Caching.Memory;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using TODO_List_Bot.Interfaces;
+using TODO_List_Bot.Services;
+
 namespace TODO_List_Bot.Commands;
 
-public class AddTask
+public class AddTask : ICommand
 {
+    public void SendMessage(ITelegramBotClient bot, Message message, TaskObject task = null, CallbackQuery callback = null)
+    {
+        AddTaskName(bot, message, task);
+    }
+
+    public static async Task<Message> AddNewTask(ITelegramBotClient bot, Message message)
+    {
+        HandleUpdateService._cache.Set("Action" + message.From.Id, "addTask_");
+        
+        return await bot.SendTextMessageAsync(chatId: message.Chat.Id,
+            text: "Введите название таска"); 
+    }
+    private async Task AddTaskName(ITelegramBotClient bot, Message message, TaskObject task)
+    {
+        HandleUpdateService._cache.Remove("Action" + message.From.Id);
+        HandleUpdateService.tasks.Add(new TaskObject(message.Text));
+        
+        await bot.SendTextMessageAsync(chatId: message.Chat.Id,
+            text: "Название таска: " + message.Text);
+        
+    }
+    //@TODO добавить поддержку времени
+    // private async Task AddTaskDate(ITelegramBotClient bot, Message message, TaskObject task)
+    // {
+    //     
+    // }
+    // private async Task AddTaskTime(ITelegramBotClient bot, Message message, TaskObject task)
+    // {
+    //     
+    // }
     
 }
