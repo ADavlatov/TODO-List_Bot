0fa0e3eb5e8b203b2942baaa0ae87532a065fc84
ADavlatov <a@davlatov.space>
2022-08-06 00:12:53 +0300

    Изменил архитектуру

--- TODO-List_Bot/Interfaces/IAddTaskCommand.cs
+++
@@ -1,9 +0,0 @@
-using Telegram.Bot;
-using Telegram.Bot.Types;
-
-namespace TODO_List_Bot.Interfaces;
-
-public interface IAddTaskCommand
-{
-    void Add(ITelegramBotClient bot, Message message);
-}
